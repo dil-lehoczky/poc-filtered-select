7b754d6c15dd06388d586a4e732865184f0f7f5e
Zoltán Lehoczky <zlehoczky@diligent.com>
2026-01-21 14:22:13 +0100

    remove router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [],
+  template: ` <h1>Welcome to !</h1> `,
   styles: [],
 })
-export class App {
-  protected readonly title = signal('poc-filtered-select');
-}
+export class App {}
