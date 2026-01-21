3b9da454ae480b64da7809b819e1662076d61f09
Zoltán Lehoczky <zlehoczky@diligent.com>
2026-01-21 17:45:23 +0100

    unused imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AppointeeService } from './appointee/appointee.service';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Appointee } from './appointee/appointee.model';
 import { MatIconModule } from '@angular/material/icon';
 import { JsonPipe } from '@angular/common';
-import {
-  catchError,
-  debounceTime,
-  distinctUntilChanged,
-  finalize,
-  of,
-  Subject,
-  switchMap,
-} from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
